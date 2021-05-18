75b651a131a985e7a2243dbe2cbabbd5333ab524
lq <zze@live.com>
2021-05-18 22:45:28 +0800

    update

--- src/Identity.API/Controllers/AccountController.cs
+++ src/Identity.API/Controllers/AccountController.cs
@@ -13,7 +13,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
