d0e1eb4fb3393a8174b315efe1295b37d7c92bde
qing long <zze@live.com>
2021-06-04 16:09:46 +0800

    update

--- src/Identity.API/Infrastructure/Extensions/HostExtensions.cs
+++ src/Identity.API/Infrastructure/Extensions/HostExtensions.cs
@@ -6,31 +6,31 @@ using Microsoft.Extensions.Logging;
 
 namespace Identity.API.Infrastructure.Extensions
 {
-  public static class HostExtensions
-  {
-    public static IHost MigrateDbContext<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder)
-      where TContext : DbContext
+    public static class HostExtensions
     {
+        public static IHost MigrateDbContext<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder)
+          where TContext : DbContext
+        {
 
-      using (var scope = host.Services.CreateScope())
-      {
-        var services = scope.ServiceProvider;
-        var logger = services.GetRequiredService<ILogger<TContext>>();
-        var context = services.GetService<TContext>();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
 
-        logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
-        InvokeSeeder(seeder, context, services);
+                logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
+                InvokeSeeder(seeder, context, services);
 
-      }
-      return host;
+            }
+            return host;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context,
+          IServiceProvider services)
+          where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
     }
-
-    private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context,
-      IServiceProvider services)
-      where TContext : DbContext
-    {
-      context.Database.Migrate();
-      seeder(context, services);
-    }
-  }
 }
